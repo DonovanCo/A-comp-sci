f559706078935137c8df617f708ce8abe32a01b5
cole.donovan <cole.donovan@MU-C3-10.mullen.pvt.k12.co.us>
2015-05-22 08:09:00 -0600

    initial

---
+++ A+ comp sci/src/FiveStates.java
@@ -0,0 +1,9 @@
+
+public class FiveStates {
+
+	public static void main(String[] args) {
+		System.out.println("ALASKA\nTEXAS\nNEW MEXICO\nOKLAHOMA\nFLORIDA");
+
+	}
+
+}
---
+++ A+ comp sci/src/LoopsBy3s.java
@@ -0,0 +1,10 @@
+
+public class LoopsBy3s {
+
+	public static void main(String[] args) {
+		
+		for(int y = 500;)
+
+	}
+
+}
